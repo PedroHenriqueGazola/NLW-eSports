dfd15357364ee7401bd0f195b587a6a4c87551c5
Pedro Gazola <ph-gazola@hotmail.com>
2022-09-12 23:19:29 -0300

    Inicializaçao do Projeto NLW eSports

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+            'id': 1,
+            'name': 'Anuncio 1'
+        },
+        {
+            'id': 2,
+            'name': 'Anuncio 2'
+        },
+        {
+            'id': 3,
+            'name': 'Anuncio 3'
+        },
+        {
+            'id': 4,
+            'name': 'Anuncio 4'
+        }
+    ])
+})
+
+app.listen(3000, () => {
+    console.log('Listening 3000')
+})
